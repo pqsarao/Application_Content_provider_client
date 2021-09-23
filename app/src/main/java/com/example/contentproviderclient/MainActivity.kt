20ce39990d1a6ea93830b3fe5cbe06e581953aeb
pqsarao <sara.alves@outlook.com>
2021-09-23 19:42:37 +0100

    Update MainActivity.kt

--- app/src/main/java/com/example/contentproviderclient/MainActivity.kt
+++ app/src/main/java/com/example/contentproviderclient/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.contentproviderclient
 
+import android.database.Cursor
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
@@ -10,13 +13,30 @@ class MainActivity : AppCompatActivity() {
     lateinit var notesRecycler: RecyclerView
     lateinit var notesRefreshButton: FloatingActionButton
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         notesRefreshButton = findViewById(R.id.client_button_refresh)
         notesRecycler = findViewById(R.id.client_list)
+        getContentProvider()
 
         notesRefreshButton.setOnClickListener {  }
+    }
+
+    private fun getContentProvider(){
+        try{
+            val url = "content://com.bootcamp.provider/notes"
+            val data = Uri.parse(url)
+            val cursor: Cursor? =
+                contentResolver.query(data, null, null, null, "title")
+            notesRecycler.apply{
+                layoutManager = LinearLayoutManager(this@MainActivity)
+                adapter = ClientAdapter(cursor as Cursor)
+            }
+        }catch (ex: Exception){
+            ex.printStackTrace()
+        }
     }
 }
